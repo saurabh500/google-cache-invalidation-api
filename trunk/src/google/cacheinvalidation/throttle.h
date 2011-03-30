e84c2f3b238554592dac88c9cb6fe3ac72ce8311
ghcooper@gmail.com <ghcooper@gmail.com@1cc9d426-c294-39be-ba72-c0199ca0f247>
2011-03-30 23:16:39 +0000

    replace references to base/scoped_ptr.h to google/cacheinvalidation/scoped_ptr.h

--- trunk/src/google/cacheinvalidation/throttle.h
+++ trunk/src/google/cacheinvalidation/throttle.h
@@ -1,4 +1,4 @@
-// Copyright 2010 Google Inc.
+// Copyright 2011 Google Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,8 +19,8 @@
 #include <deque>
 #include <vector>
 
-#include "base/scoped_ptr.h"
 #include "google/cacheinvalidation/invalidation-client.h"
+#include "google/cacheinvalidation/scoped_ptr.h"
 #include "google/cacheinvalidation/stl-namespace.h"
 #include "google/cacheinvalidation/time.h"
 
