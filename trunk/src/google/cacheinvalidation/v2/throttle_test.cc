f5c64b01bea56371ed4cf61234a038e57a0a1caf
ghcooper@gmail.com <ghcooper@gmail.com@1cc9d426-c294-39be-ba72-c0199ca0f247>
2012-01-09 22:23:39 +0000

    update googletest.h path in throttle_test

--- trunk/src/google/cacheinvalidation/v2/throttle_test.cc
+++ trunk/src/google/cacheinvalidation/v2/throttle_test.cc
@@ -1,4 +1,4 @@
-// Copyright 2011 Google Inc.
+// Copyright 2012 Google Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
 
 // Tests the throttle.
 
-#include "google/cacheinvalidation/googletest.h"
+#include "google/cacheinvalidation/v2/googletest.h"
 #include "google/cacheinvalidation/v2/test/deterministic-scheduler.h"
 #include "google/cacheinvalidation/v2/throttle.h"
 
