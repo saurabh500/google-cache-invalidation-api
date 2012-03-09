4c181abaa8f918496fbd647be4be7c0c431c4679
ghcooper <ghcooper@gmail.com>
2012-03-09 00:25:20 +0000

    * Moved dependencies to ticl/deps * Removed v2 in the directory names, except for protos. * The directory now has deps, impl, include, test

---
+++ src/google/cacheinvalidation/impl/simple-registration-store.cc
@@ -0,0 +1,79 @@
+// Copyright 2012 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Simple, map-based implementation of DigestStore.
+
+#include "google/cacheinvalidation/impl/simple-registration-store.h"
+
+#include "google/cacheinvalidation/impl/object-id-digest-utils.h"
+
+namespace invalidation {
+
+void SimpleRegistrationStore::Add(const ObjectIdP& oid) {
+  registrations_[ObjectIdDigestUtils::GetDigest(oid, digest_function_)] = oid;
+  RecomputeDigest();
+}
+
+void SimpleRegistrationStore::Add(const vector<ObjectIdP>& oids) {
+  for (size_t i = 0; i < oids.size(); ++i) {
+    const ObjectIdP& oid = oids[i];
+    registrations_[ObjectIdDigestUtils::GetDigest(oid, digest_function_)] = oid;
+  }
+  RecomputeDigest();
+}
+
+void SimpleRegistrationStore::Remove(const ObjectIdP& oid) {
+  registrations_.erase(ObjectIdDigestUtils::GetDigest(oid, digest_function_));
+  RecomputeDigest();
+}
+
+void SimpleRegistrationStore::Remove(const vector<ObjectIdP>& oids) {
+  for (size_t i = 0; i < oids.size(); ++i) {
+    const ObjectIdP& oid = oids[i];
+    registrations_.erase(ObjectIdDigestUtils::GetDigest(oid, digest_function_));
+  }
+  RecomputeDigest();
+}
+
+void SimpleRegistrationStore::RemoveAll(vector<ObjectIdP>* oids) {
+  for (map<string, ObjectIdP>::const_iterator iter = registrations_.begin();
+       iter != registrations_.end(); ++iter) {
+    oids->push_back(iter->second);
+  }
+  registrations_.clear();
+  RecomputeDigest();
+}
+
+bool SimpleRegistrationStore::Contains(const ObjectIdP& oid) {
+  return registrations_.find(
+      ObjectIdDigestUtils::GetDigest(oid, digest_function_)) !=
+      registrations_.end();
+}
+
+void SimpleRegistrationStore::GetElements(
+    const string& oid_digest_prefix, int prefix_len,
+    vector<ObjectIdP>* result) {
+  // We always return all the registrations and let the Ticl sort it out.
+  for (map<string, ObjectIdP>::iterator iter = registrations_.begin();
+       iter != registrations_.end(); ++iter) {
+    result->push_back(iter->second);
+  }
+}
+
+void SimpleRegistrationStore::RecomputeDigest() {
+  digest_ = ObjectIdDigestUtils::GetDigest(
+      registrations_, digest_function_);
+}
+
+}  // namespace invalidation
