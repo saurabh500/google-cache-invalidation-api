5bf2e766da73c1f883e90e02433e37aa4382cb6f
ghcooper <ghcooper@gmail.com>
2012-03-10 00:20:25 +0000

    fix include for invalidation client factory

--- src/google/cacheinvalidation/impl/invalidation-client-factory.cc
+++ src/google/cacheinvalidation/impl/invalidation-client-factory.cc
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "google/cacheinvalidation/include/invalidation-client.h"
+#include "google/cacheinvalidation/include/invalidation-client-factory.h"
 
 #include "google/cacheinvalidation/impl/invalidation-client-impl.h"
 
